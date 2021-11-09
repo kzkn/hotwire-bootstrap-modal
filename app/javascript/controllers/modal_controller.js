aa7e1b110957c011501e5dca8db0c57b48763dd2
Kazuki Nishikawa <kz.nishikawa@gmail.com>
2021-11-09 22:52:03 +0900

    modal

--- app/javascript/controllers/modal_controller.js
+++ app/javascript/controllers/modal_controller.js
@@ -5,6 +5,16 @@ import * as bootstrap from 'bootstrap'
 export default class extends Controller {
   static targets = ["container"]
 
+  connect() {
+    this.element.addEventListener('turbo:before-fetch-response', this.populate)
+    this.element.addEventListener('turbo:submit-start', this.beforeSend)
+  }
+
+  disconnect() {
+    this.element.removeEventListener('turbo:before-fetch-response', this.populate)
+    this.element.removeEventListener('turbo:submit-start', this.beforeSend)
+  }
+
   open(ev) {
     ev.preventDefault()
 
@@ -15,15 +25,17 @@ export default class extends Controller {
       dataType: 'html',
       beforeSend: (xhr, options) => {
         xhr.setRequestHeader('X-Show-In-Modal', true)
+        return true
       },
       success: (data) => {
-        console.log('hoge', data)
         if (this.hasContainerTarget) {
           this.containerTarget.replaceWith(data.body.firstChild)
         } else {
           this.element.appendChild(data.body.firstChild)
         }
-        this.openModal()
+
+        this.modal = new bootstrap.Modal(this.containerTarget)
+        this.modal.show()
       },
       error: (data) => {
         console.log('error', data)
@@ -31,8 +43,24 @@ export default class extends Controller {
     })
   }
 
-  openModal() {
-    const modal = bootstrap.Modal(this.containerTarget)
-    modal.show()
+  beforeSend = (event) => {
+    const { detail: { formSubmission } } = event
+    formSubmission.fetchRequest.headers['X-Show-In-Modal'] = true
+  }
+
+  populate = async (event) => {
+    const { detail: { fetchResponse } } = event
+    if (!fetchResponse.succeeded) {
+      event.preventDefault()
+      const html = await fetchResponse.responseText
+      this.replaceModalContent(html)
+    }
+  }
+
+  replaceModalContent(html) {
+    const fragment = document.createElement('div')
+    fragment.innerHTML = html
+    const newContent = fragment.querySelector('.modal-content')
+    this.containerTarget.querySelector('.modal-content').replaceWith(newContent)
   }
 }
